3b66408222772b276a597abed16d8b434e5cf25f
feedmegg2-dotcom <feedme.gg2@gmail.com>
2026-06-01 12:26:06 +0100

    Update page.tsx

--- src/app/restaurant/[slug]/page.tsx
+++ src/app/restaurant/[slug]/page.tsx
@@ -1,19 +1,1 @@
--- Allow public to read menu categories
-CREATE POLICY "Menu categories are public" ON menu_categories
-  FOR SELECT USING (is_active = true);
 
--- Allow public to read delivery zones
-CREATE POLICY "Delivery zones are public" ON delivery_zones
-  FOR SELECT USING (is_active = true);
-
--- Allow public to read opening hours
-CREATE POLICY "Opening hours are public" ON opening_hours
-  FOR SELECT USING (true);
-
--- Allow public to read modifier groups
-CREATE POLICY "Modifier groups are public" ON modifier_groups
-  FOR SELECT USING (true);
-
--- Allow public to read modifier options
-CREATE POLICY "Modifier options are public" ON modifier_options
-  FOR SELECT USING (true);
