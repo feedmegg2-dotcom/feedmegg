16ee45261b617f94b4f0ef52048b4f0312fe1262
feedmegg2-dotcom <feedme.gg2@gmail.com>
2026-06-29 21:52:21 +0100

    Update usePrinterAutoprint.ts

--- src/hooks/usePrinterAutoprint.ts
+++ src/hooks/usePrinterAutoprint.ts
@@ -95,14 +95,13 @@ function renderElementESCPOS(el: any, order: OrderForPrint, cols: number): strin
       })
       break
     case 'items_list_with_numbers': {
-      // kitchen_number_size maps to ESC/POS size multipliers
       const knSize = el.kitchen_number_size || 'xxxl'
-      // GS ! n: bits 0-2 = width multiplier-1, bits 4-6 = height multiplier-1
-      // xxxl/xxl = 4x = 0x33, xl = 3x = 0x22, large = 2x = 0x11, medium = 1x = 0x00
-      const knCmd = knSize === 'xxxl' ? GS + '!3' :
-                    knSize === 'xxl'  ? GS + '!3' :
-                    knSize === 'xl'   ? GS + '!"' :
-                    knSize === 'large'? GS + '!' : SIZE_NORMAL
+      // GS ! byte: high nibble = height mult-1, low nibble = width mult-1
+      // 4x4=0x33, 3x3=0x22, 2x2=0x11, 1x1=0x00
+      const knByte = knSize === 'xxxl' || knSize === 'xxl' ? 0x33 :
+                     knSize === 'xl' ? 0x22 :
+                     knSize === 'large' ? 0x11 : 0x00
+      const knCmd = GS + '!' + String.fromCharCode(knByte)
       order.items.forEach((item: any) => {
         if (item.kitchen_number) {
           t += ALIGN_CENTER + knCmd + BOLD_ON + '[' + item.kitchen_number + ']' + BOLD_OFF + SIZE_NORMAL + LF
